36f16bad08e808a6f1ac39dc7bc280bd2a9a075b
isaac <isaaccastrocisneros@gmail.com>
2023-04-10 12:43:50 -0500

    hmmm

--- app/(global-context)/(main-ui)/certificados/[id]/page.tsx
+++ app/(global-context)/(main-ui)/certificados/[id]/page.tsx
@@ -6,11 +6,9 @@ const TableEdit = dynamic(() => import("./components/TableEdit"), { ssr: false }
 export default function EditCertificado({ params }: any) {
     const { id } = params
     return(
-        <>{id}</>
-    )
-}
-
-{/* <>
+        <>
             <h1 className='Montserrat font-black text-2xl mb-5'>Cursos Inscritos de Juan Perez</h1>
             <TableEdit id={id} />
-        </> */}
+        </>
+    )
+}
