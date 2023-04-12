0862130633a0c4400c44faded613e972442b70f3
isaac <isaaccastrocisneros@gmail.com>
2023-04-12 12:13:17 -0500

    heeey

--- app/(global-context)/(main-ui)/dashboard/layout.tsx
+++ app/(global-context)/(main-ui)/dashboard/layout.tsx
@@ -11,8 +11,6 @@ export default async function RootLayout({
 
     return (
       <>
-        <Link href={'/lel'}>rrrr</Link>
-        <Link href={'/login'}>rrrr</Link>
         <MyPopUp />
         <header className="w-[5rem] mob:w-[3rem] h-[100%] relative z-[999]">
            <NavbarUltimate />
