275c317f3719ee8393dc979db54e301fc378320f
isaac <isaaccastrocisneros@gmail.com>
2023-04-12 11:42:43 -0500

    hmmm

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 import { redirect } from 'next/navigation';
 
-export const config = { matcher: ["/hey"] }
+export const config = { matcher: ["/dashboard/:path*"] }
 
