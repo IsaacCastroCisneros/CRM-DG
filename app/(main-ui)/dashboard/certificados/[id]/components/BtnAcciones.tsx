ef0090feb3fe94fb0e606938343ef20b2a99f441
isaac <isaaccastrocisneros@gmail.com>
2023-04-20 10:40:03 -0500

    lel

--- app/(main-ui)/dashboard/certificados/[id]/components/BtnAcciones.tsx
+++ app/(main-ui)/dashboard/certificados/[id]/components/BtnAcciones.tsx
@@ -14,9 +14,36 @@ export const BtnAcciones = ({ data, buscarData, tipo }: any) => {
           {({ open }) => (
             <>
               {tipo === "Curso" ? (
-                <Popover.Button className="bg-green-500 p-2 text-white rounded-md">
-                  <span>Descargar</span>
-                </Popover.Button>
+                  <div className="bg-gray-50 flex gap-2">
+                    <PDFDownloadLink
+                      document={
+                        <CertificadoCurso
+                          data={data}
+                          nombre={buscarData?.nombre}
+                          tipo={"nota"}
+                        />
+                      }
+                      fileName={`${buscarData?.nombre}`}
+                    >
+                      <button className="p-2 bg-blue-500 text-white rounded-md font-bold">
+                        CON NOTA
+                      </button>
+                    </PDFDownloadLink>
+                    <PDFDownloadLink
+                      document={
+                        <CertificadoCurso
+                          data={data}
+                          nombre={buscarData?.nombre}
+                          tipo={"sinNota"}
+                        />
+                      }
+                      fileName={`${buscarData?.nombre}`}
+                    >
+                      <button className="p-2 bg-red-500 text-white rounded-md font-bold">
+                        SIN NOTA
+                      </button>
+                    </PDFDownloadLink>
+                  </div>
               ) : (
                 <PDFDownloadLink
                   document={
@@ -32,50 +59,6 @@ export const BtnAcciones = ({ data, buscarData, tipo }: any) => {
                   </button>
                 </PDFDownloadLink>
               )}
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-1"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-1"
-              >
-                <Popover.Panel className="fixed z-10 w-auto -translate-x-1/2 transform px-4 sm:px-0">
-                  <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                    <div className="bg-gray-50 p-4 flex gap-2">
-                    <PDFDownloadLink
-                        document={
-                          <CertificadoCurso
-                            data={data}
-                            nombre={buscarData?.nombre}
-                            tipo={"nota"}
-                          />
-                        }
-                        fileName={`${buscarData?.nombre}`}
-                      >
-                        <button className="p-2 bg-blue-500 text-white rounded-md font-bold">
-                          CON NOTA
-                        </button>
-                      </PDFDownloadLink>
-                      <PDFDownloadLink
-                        document={
-                          <CertificadoCurso
-                            data={data}
-                            nombre={buscarData?.nombre}
-                            tipo={"sinNota"}
-                          />
-                        }
-                        fileName={`${buscarData?.nombre}`}
-                      >
-                        <button className="p-2 bg-red-500 text-white rounded-md font-bold">
-                          SIN NOTA
-                        </button>
-                      </PDFDownloadLink>
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
             </>
           )}
         </Popover>
--- app/(main-ui)/dashboard/certificados/components/TableCertificados.tsx
+++ app/(main-ui)/dashboard/certificados/components/TableCertificados.tsx
@@ -131,4 +131,3 @@ export const TableCertificados = () => {
     )
 }   
 
-/* fdfd */
